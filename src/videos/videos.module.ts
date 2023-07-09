f2035d3333dc5df7ac0ed701f03b6ba36f1695ec
Darren Atienza <datienza@doehle-mnl.com>
2023-07-09 17:24:36 +0800

    #4 update videos entity

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VideosService } from './services/videos.service';
 import { VideosController } from './controller/videos.controller';
+import { Video } from './model/video.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Video])],
   providers: [VideosService],
   controllers: [VideosController],
 })
